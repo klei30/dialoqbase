75665c1d2802537d5977bb4694a9d4b5b50293f3
klei aliaj <121291603+klei30@users.noreply.github.com>
2023-12-07 19:53:39 +0100

    Update BotPlaygroundLayout.tsx

--- app/ui/src/Layout/BotPlaygroundLayout.tsx
+++ app/ui/src/Layout/BotPlaygroundLayout.tsx
@@ -235,7 +235,7 @@ export default function BotPlaygroundLayout({
               className="focus:outline-none focus-visible:ring-2 focus-visible:ring-pink-700 flex items-center px-3"
             >
               <img className="h-8 w-auto" src="/logo.png" alt="Dialoqbase" />
-              <span className="ml-1 text-xl font-bold">Dialoqbase</span>
+              <span className="ml-1 text-xl font-bold">Dialogo</span>
               <span className="inline-block flex-shrink-0 rounded-full bg-green-100 px-2 py-0.5 text-xs font-medium text-green-800 ml-2">
                 {/* @ts-ignore */}
                 {`v${__APP_VERSION__}`}
